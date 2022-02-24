c50f77781a51f7cd24474370928a0a7dfb2621b2
Sebastian  Varona <sebastian@MacBook-Pro-de-Sebastian.local>
2022-02-24 00:27:06 -0500

    set english as default lang

--- components/navbar.js
+++ components/navbar.js
@@ -83,7 +83,7 @@ export default function Navbar(props) {
                             </svg>
                             <span className="mx-1">{`${props.lang === 'en' ? 'En' : 'Es'}`}</span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down place-self-end pb-1" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                                <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                             </svg>
                         </button>
                         <div className={ langDisplay ? "flex flex-col justify-center absolute right-0 top-14 bg-border rounded-md overflow-hidden" : "hidden" }>
