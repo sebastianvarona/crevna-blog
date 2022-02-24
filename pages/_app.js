1a3f16db28235497d5788364f51416bf8d0e31ff
Sebastian  Varona <sebastian@MacBook-Pro-de-Sebastian.local>
2022-02-24 12:32:52 -0500

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -7,13 +7,13 @@ function MyApp({ Component, pageProps }) {
         <>
         {/* Global site tag (gtag.js) - Google Analytics */}
         <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-        <Script strategy="lazyOnload">
+        <Script id='google-analytics' strategy="lazyOnload">
             {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments)}
             gtag('js', new Date());
             
-            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
             `}
         </Script>
         <Component {...pageProps} />
